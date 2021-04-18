75c870341365e8f530969e719cb06162261f269d
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-04-19 02:53:50 +0300

    Add random and allcolor methods

--- index.js
+++ index.js
@@ -3,59 +3,84 @@ module.exports = class Colorize extends console.Console {
   constructor() {
     super(process.stdout, process.stderr);
     this.styles = {
+      fgBlack: '\x1b[30m',
+      fgRed: '\x1b[31m',
+      fgGreen:  '\x1b[32m',
+      fgYellow:  '\x1b[33m',
+      fgBlue:  '\x1b[34m',
+      fgMagenta:  '\x1b[35m',
+      fgCyan:  '\x1b[36m',
+      fgWhite:  '\x1b[37m',
       reset: '\x1b[0m',
-      bright: '\x1b[1m%s',
-      dim: '\x1b[2m%s',
-      underscore: '\x1b[4m%s',
-      blink: '\x1b[5m%s',
-      reverse: '\x1b[7m%s',
-      hidden: '\x1b[8m%s',
-      fgBlack: '\x1b[30m%s',
-      fgRed: '\x1b[31m%s',
-      fgGreen:  '\x1b[32m%s',
-      fgYellow:  '\x1b[33m%s',
-      fgBlue:  '\x1b[34m%s',
-      fgMagenta:  '\x1b[35m%s',
-      fgCyan:  '\x1b[36m%s',
-      fgWhite:  '\x1b[37m%s',
-      bgBlack:  '\x1b[40m%s',
-      bgRed:  '\x1b[41m%s',
-      bgGreen:  '\x1b[42m%s',
-      bgYellow:  '\x1b[43m%s',
-      bgBlue:  '\x1b[44m%s',
-      bgMagenta:  '\x1b[45m%s',
-      bgCyan:  '\x1b[46m%s',
-      bgWhite: '\x1b[47m%s',
+      bright: '\x1b[1m',
+      dim: '\x1b[2m',
+      underscore: '\x1b[4m',
+      blink: '\x1b[5m',
+      reverse: '\x1b[7m',
+      hidden: '\x1b[8m',
+      bgBlack:  '\x1b[40m',
+      bgRed:  '\x1b[41m',
+      bgGreen:  '\x1b[42m',
+      bgYellow:  '\x1b[43m',
+      bgBlue:  '\x1b[44m',
+      bgMagenta:  '\x1b[45m',
+      bgCyan:  '\x1b[46m',
+      bgWhite: '\x1b[47m',
+      [Symbol.iterator]: function* (){
+        const keys = Object.keys(this);
+        for (key of keys){
+          yield this[key];
+        }
+        return true;
+      },
+      randomColor() {
+        const keys = Object.keys(this);
+        return this[keys[Math.floor(Math.random() * (keys.length - 1))]];
+      },
     };
   }
 
   styled(style, ...args) {
     args.push(this.styles.reset);
-    super.log(style, ...args);
+    super.log(style + '%s', ...args);
   }
 
-  green(output) {
-    this.styled(this.styles.fgGreen, output);
+  green(string) {
+    this.styled(this.styles.fgGreen, string);
   }
 
-  red(output) {
-    this.styled(this.styles.fgRed, output);
+  red(string) {
+    this.styled(this.styles.fgRed, string);
   }
 
-  yellow(output) {
-    this.styled(this.styles.fgYellow, output);
+  yellow(string) {
+    this.styled(this.styles.fgYellow, string);
   }
 
-  blue(output) {
-    this.styled(this.styles.fgBlue, output);
+  blue(string) {
+    this.styled(this.styles.fgBlue, string);
   }
 
-  magenta(output) {
-    this.styled(this.styles.fgMagenta, output);
+  magenta(string) {
+    this.styled(this.styles.fgMagenta, string);
   }
 
-  cyan(output) {
-    this.styled(this.styles.fgCyan, output);
+  cyan(string) {
+    this.styled(this.styles.fgCyan, string);
   }
-
+  random(string) {
+    this.styled(this.styles.randomColor(), string);
+  }
+  allColors(string) {
+    const keys = Object.keys(this.styles);
+    const newKeys = keys.filter(el => keys.indexOf(el) < 8);
+    const letters = string.split('');
+    let newStr = '';
+    for(let letter of letters){
+      letter = this.styles[newKeys[Math.floor(Math.random() * newKeys.length)]] + letter;
+      newStr += letter
+    }
+    newStr += this.styles.reset;
+    super.log(newStr);
+  }
 };
